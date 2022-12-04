f7eea2f6237b4089196f9024460c8e09ff8ead9d
jonbonde <jhdank@icloud.com>
2022-12-04 18:36:20 +0100

    Lagt til dag 4

---
+++ Day4/Part2.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+public class Part2 {
+    public static void main(String[] args) {
+        try (RandomAccessFile file = new RandomAccessFile("input.txt", "r")) {
+            String str;
+
+            int overlap = 0;
+            
+            while ((str = file.readLine()) != null) {
+                String[] pair = str.split(",");
+                String[] venstreStr = pair[0].split("-");
+                String[] hoyreStr = pair[1].split("-");
+
+                int[] venstre = {Integer.parseInt(venstreStr[0]), Integer.parseInt(venstreStr[1])};
+                int[] hoyre = {Integer.parseInt(hoyreStr[0]), Integer.parseInt(hoyreStr[1])};
+                
+                if ((venstre[0] <= hoyre[0] && venstre[1] >= hoyre[0]) || (hoyre[0] <= venstre[0] && hoyre[1] >= venstre[0])) {
+                    overlap += 1;
+                }
+            }
+            System.out.println(overlap);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
